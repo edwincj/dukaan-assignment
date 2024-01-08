a928456d8a2cd96b03d732e2b17bc9df869c4b68
edwincj <edwincj1998@gmail.com>
2024-01-08 19:11:57 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
