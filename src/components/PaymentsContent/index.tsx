2a9136ae056487db5bd734976a0fd1610e55d3a1
edwincj <edwincj1998@gmail.com>
2024-01-09 16:51:06 +0530

    fix: padding under the table correction

--- src/components/PaymentsContent/index.tsx
+++ src/components/PaymentsContent/index.tsx
@@ -2,7 +2,7 @@ import PaymentsOverview from 'components/PaymentsOverview';
 import PaymentsTable from 'components/PaymentsTable';
 
 const PaymentsContent = () => (
-  <div className="flex-1 bg-[#FAFAFA] px-8">
+  <div className="flex-1 bg-[#FAFAFA] px-8 pb-8">
     <PaymentsOverview />
     <PaymentsTable />
   </div>
