1578362f93f2af18a8c3cc0e402d34c6d508b0dd
edwincj <edwincj1998@gmail.com>
2024-01-09 16:49:03 +0530

    fix: added padding under the table

--- src/pages/Payments/index.tsx
+++ src/pages/Payments/index.tsx
@@ -2,7 +2,7 @@ import PaymentsContent from 'components/PaymentsContent';
 import PaymentsHeader from 'components/PaymentsHeader';
 
 const Payments = () => (
-  <div className="flex-1 flex flex-col overflow-auto">
+  <div className="flex-1 flex flex-col overflow-auto pb-8">
     <PaymentsHeader />
     <PaymentsContent />
   </div>
